469ca630dc90a127fff26477da5dae5c2b5b851a
noah1400 <48360809+noah1400@users.noreply.github.com>
2022-11-21 16:51:30 +0100

    Update main.c

--- PGM/Labor_6/A06/main.c
+++ PGM/Labor_6/A06/main.c
@@ -1,5 +1,5 @@
 int stringlength(const char *str) {
     const char *ptr = str;
-    while(*(ptr++) != '\0');
-    return ptr-1-str;
+    while(*(ptr++) != '\0'); // Increment ptr until it points to the null character
+    return ptr-1-str; // Return the difference between the pointers
 }
