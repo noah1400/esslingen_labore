e294c6c6a44156ac9b1efa34a721fff3f8e46abe
noah1400 <48360809+noah1400@users.noreply.github.com>
2022-11-29 21:24:29 +0100

    Update calc.py

--- DB/Labor_4/calculations/calc.py
+++ DB/Labor_4/calculations/calc.py
@@ -2,7 +2,7 @@ import math
 number_of_rows = [300, 200, 800, 25, 100]
 
 # DB2 Datatype sizes in bytes
-varchar_size = lambda l: l
+varchar_size = lambda l: l+2
 smallint_size = 2
 date_size = 4
 decimal_size = 6
@@ -21,7 +21,9 @@ full_row_size_stock = smallint_size+decimal_size+int_size
 full_row_size_reorder = smallint_size+timestamp_size
 full_row_size_author = smallint_size+varchar_size(50)+char_size+CLOB_size+BLOB_size
 
-avg_row_size = lambda l, nor: math.ceil(l/nor)
+
+
+avg_row_size = lambda l: l
 
 books_avg_key_size = 2
 speaker_avg_key_size = 0
@@ -32,8 +34,8 @@ author_avg_key_size = 27
 
 
 def calc_rows_per_page(ars): # average row size
-    print("ars: ",ars)
-    return math.floor(4028/(ars+10))
+    rpp = math.floor(4028/(ars+10))
+    return rpp
 
 def calc_number_of_pages(tnor, rpp): # total number of rows, rows per page
     return math.ceil((tnor/rpp)*1.1)
@@ -47,7 +49,7 @@ def calc_lob_data_obj(lcs, norwld): # lob column size, number of rows with lob d
 def calc_alloc_obj(ldo): # lob data object
     return (math.ceil(ldo/65536000000)+math.ceil(ldo/8192000)*4096)
 
-books_ars = avg_row_size(full_row_size_books,3) # average row size
+books_ars = full_row_size_books # average row size
 books_rpp = calc_rows_per_page(books_ars) # rows per page
 books_nor = number_of_rows[0] # number of rows
 books_nop = calc_number_of_pages(books_nor, books_rpp) # number of pages
@@ -55,28 +57,28 @@ books_is = calc_index_size(books_avg_key_size, books_nor) # index size
 books_lobs = 0 # LOB Size
 
 
-speaker_ars = avg_row_size(full_row_size_speaker,3)
+speaker_ars = full_row_size_speaker
 speaker_rpp = calc_rows_per_page(speaker_ars)
 speaker_nor = number_of_rows[1]
 speaker_nop = calc_number_of_pages(speaker_nor, speaker_rpp)
 speaker_is = calc_index_size(speaker_avg_key_size, speaker_nor)
 speaker_lobs = 0
 
-stock_ars = avg_row_size(full_row_size_stock,3)
+stock_ars = full_row_size_stock
 stock_rpp = calc_rows_per_page(stock_ars)
 stock_nor = number_of_rows[2]
 stock_nop = calc_number_of_pages(stock_nor, stock_rpp)
 stock_is = calc_index_size(stock_avg_key_size, stock_nor)
 stock_lobs = 0
 
-reorder_ars = avg_row_size(full_row_size_reorder, 2)
+reorder_ars = full_row_size_reorder
 reorder_rpp = calc_rows_per_page(reorder_ars)
 reorder_nor = number_of_rows[3]
 reorder_nop = calc_number_of_pages(reorder_nor, reorder_rpp)
 reorder_is = calc_index_size(reorder_avg_key_size, reorder_nor)
 reorder_lobs = 0
 
-author_ars = avg_row_size(full_row_size_author, 5)
+author_ars = full_row_size_author
 author_rpp = calc_rows_per_page(author_ars)
 author_nor = number_of_rows[4]
 author_nop = calc_number_of_pages(author_nor, author_rpp)
