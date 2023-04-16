5aeb4b44f8cba10e747791b512f46b2da35d02c9
Noah Scholz <48360809+noah1400@users.noreply.github.com>
2023-04-16 20:29:20 +0200

    Update delay.asm

--- CA/CA3_Lab1/lab1-lcdVorlage/Sources/delay.asm
+++ CA/CA3_Lab1/lab1-lcdVorlage/Sources/delay.asm
@@ -1,18 +1,25 @@
 
 
 delay_0_5sec:
+    PSHY
+    PSHX
 
-    PSHX      ; push x to stack
-    PSHY      ; push y to stack
+    LDY #50
+    del1:
+        DEY 
+        CPY #0
+        LDX #4000
+        BRA delay
+        BNE del1
+
+    PULY
+    PULY
+    RTS
     
-    LDY       #50000
-    LDX       #20
-    delay_loop:
-    DBNE      Y, delay_loop
-    LDY       #50000
-    DBNE      X, delay_loop
-    
-    PULY      ; restore y and x by pulling from stack
-    PULX
-    
-    RTS       ;  return from subroutine
+
+delay:     
+    dex                   ; 1 cycle
+    inx                   ; 1 cycle
+    dex                   ; 1 cycle
+    bne   del1            ; 3 cylce
+    rts
