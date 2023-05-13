0a8123d9c655a8e03fd814ddb9b230bc2fcddcc8
noah1400 <48360809+noah1400@users.noreply.github.com>
2023-05-13 18:33:19 +0200

    update clock.asm

--- CA/CA3_Lab2/lab2-Uhr-C-Vorlage/Sources/clock.asm
+++ CA/CA3_Lab2/lab2-Uhr-C-Vorlage/Sources/clock.asm
@@ -3,6 +3,8 @@
     XDEF scnds, mins, hrs, time
     
     XREF initLED, clrLED, toggleLED
+    XREF decToASCII
+    XREF writeLine
     
 .data: SECTION
 
@@ -57,14 +59,111 @@ tickClock:
   
   PULB
   rts
+
+exitTick:
+  PULB
+  rts
   
 updateTime:
+  CLRA
+  LDAB  scnds
+  JSR   decToASCII  ; ASCII String now in X
+  LDY   #scndsTXT
+  JSR   onlyLastTwoChars
+  
+  LDAB  mins
+  JSR   decToASCII
+  LDY   #minsTXT
+  JSR   onlyLastTwoChars
+  
+  LDAB  hrs
+  JSR   decToASCII
+  LDY   #hrsTXT
+  JSR   onlyLastTwoChars
   
   rts
   
 outputTime:
+  
+  LDX   #time
+  LDAB  #1
+  JSR   writeLine 
 
   rts
+  
+  
+onlyLastTwoChars:   ; decToASCII: "0035" -> "35"
+  ; Register X: original String
+  ; Register Y pointer to resulting string
+  PSHB
+  PSHA
+  PSHX
+  PSHY
+  LDAB  #2 ; Counter to remove two characters
+  lastTwoChars_loop:
+     LDAA   2, X
+     STAA   0, Y
+     INX
+     INY
+     DECB
+     BNE  lastTwoChars_loop
+  CLRA        ; Set the null character
+  STAA  0, Y  ; null terminate the resulting string
+  
+  PULY
+  PULX
+  PULA
+  PULB
+  rts
+  
+combineStrings:
+   ; Uses  variables scndsTXT, minsTXT, hrsTXT
+   ; And combines them to HH:MM:SS\n
+   PSHB
+   PSHA
+   PSHX
+   PSHY
+   
+   LDY  #time     ; Address to resulting string here time
+   LDX  #hrsTXT   ; Address to first string
+   JSR  combineStrings_loop
+   LDX  #minsTXT  ; Address to second string
+   JSR  combineStrings_loop
+   LDX  #scndsTXT ; Address to last string
+   JSR  combineStrings_loop
+   JSR  combineStrings_loop_end
+   
+   PULY
+   PULX
+   PULA
+   PULB
+   
+   rts
+   
+   combineStrings_loop:
+      LDAA  0, X    ; Load a character from the current string
+      CMPA  #0   ; Check if the end of the current string is reached
+      BEQ   combineStrings_loop_rts ; If yes, exit and continue with next string
+      
+      STAA  0, Y    ; Store the character in the resulting string
+      INX           ; Increment the current string pointer
+      INY           ; Increment the resulting string pointer
+      
+      LDAA  #':'    ; Load the delimiter ":"
+      STAA  0, Y    ; Store the delimiter in the resulting string
+      INY           ; Increment the resulting string pointer
+      
+      BRA   combineStrings_loop ; Repeat for the next character
+      
+   combineStrings_loop_rts:
+      rts
+      
+   combineStrings_loop_end:
+      DEY           ; Decrement the resulting string pointer to remove the last delimiter ':'
+      CLRA          ; Set the null character
+      STAA  0, Y    ; Null terminate the resulting string
+      rts
+      
   
   
 
@@ -112,6 +211,3 @@ resetHours:
 
 
   
-exitTick:
-  PULB
-  rts
