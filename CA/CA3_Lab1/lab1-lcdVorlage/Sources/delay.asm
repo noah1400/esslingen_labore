5204104962d58c87148886e9065bbe4be0027b16
Noah Scholz <48360809+noah1400@users.noreply.github.com>
2023-05-15 12:45:32 +0200

    Update delay.asm

--- CA/CA3_Lab1/lab1-lcdVorlage/Sources/delay.asm
+++ CA/CA3_Lab1/lab1-lcdVorlage/Sources/delay.asm
@@ -1,18 +1,36 @@
- XDEF delay_0_5sec
+;   Labor 1 
+;   delay 0.5 sec
+;   Subroutine hexToASCII
+;
+;   Computerarchitektur
+;   (C) 2019-2022 J. Friedrich, W. Zimmermann, R. Keller
+;   Hochschule Esslingen
+;
+;   Author:   Noah Scholz
+;   Modified: Noah Scholz, May 2023
 
+; export symbols
+  XDEF delay_0_5sec
+
+; Defines
+SPEED:  EQU     $07D0                   ; Change this number to change counting speed
+
+; ROM: Code section
+.init: SECTION
+
+; Public inferface function: hexToASCII
+;   delay the program by 0.5 seconds
+; Parameter:
+;  Y    used as counter
+;  X    used as counter
+; Returns: -
+; Registers: X, Y = 0
 delay_0_5sec:
-
-    PSHX      ; push x to stack
-    PSHY      ; push y to stack
-    
-    LDY       #50000
-    LDX       #20
-    delay_loop:
-    DBNE      Y, delay_loop
-    LDY       #50000
-    DBNE      X, delay_loop
-    
-    PULY      ; restore y and x by pulling from stack
-    PULX
-    
-    RTS       ;  return from subroutine
+    LDX #SPEED          ; 2 cycles nur 1mal
+    waitO:
+      LDY #SPEED        ; 2 cycles x-mal
+    waitI:
+      DBNE Y, waitI     ; 3 cycles x-mal y-mal
+      DBNE X, waitO     ; 3 cycles x-mal
+      RTS               ; 5 cycles
+      
